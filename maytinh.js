67c1befefeaf3c42739ee99df86f403bb8ad6592
bejun205 <223001697@daihocthudo.edu.vn>
2024-10-04 16:46:01 +0700

    Add files via upload

---
+++ maytinh.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.value === "0") {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = "0";
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Lỗi!";
+    }
+}
